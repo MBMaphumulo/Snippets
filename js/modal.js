b546ae05f6e2980787768df7fca5f9890be2fed8
MelB <Melusimaphumulo@gmail.com>
2019-11-28 14:04:33 +0200

    added card and modal desgin

---
+++ js/modal.js
@@ -0,0 +1,19 @@
+function show(element,action){
+    
+    // var header = document.getElementById('modal-header').innerHTML = "";
+    // var footer = document.getElementById('modal-footer').innerHTML = "";
+
+    $(element).fadeIn();
+
+}
+
+function hide(element){
+    $(element).fadeOut();
+}
+
+
+
+
+
+
+               
